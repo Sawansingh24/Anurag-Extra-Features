55fb25c9c784c03ca3f34fa0f756b7d84fe06b50
Sawansingh24 <119250143+Sawansingh24@users.noreply.github.com>
2023-01-19 22:01:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sawansingh24/Anurag-Extra-Features.git/ Anurag-Extra-Featureselse
+  git clone https://github.com/Sawansingh24/Anurag-Extra-Features.git/ Anurag-Extra-Features
+else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Anurag-Extra-Features
 fi
